75dc066d7f3f1f8cfb65c8cc681b0da85f09d435
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-01-05 18:43:35 -0500

    added some comments

--- schedule-maker.py
+++ schedule-maker.py
@@ -6,14 +6,17 @@ API_KEY = "?key=36802f2c7eab5943ece0bcf8eec07d5a"
 
 if __name__ == "__main__":
 
+    #assuming 5 courses, loops 0-4
 	for i in range(0, 5):
 	    print "Enter name for Course #%d (e.g CS 350)" % (i + 1,)
+        #get input
 	    currentCourse = raw_input()
 	    # error check
 	    while not currentCourse or len(currentCourse.split(' ')) != 2:	    	
         	print "Please enter a valid course name"
         	currentCourse = raw_input()
 
+        #split course into two parts
     	courseName = currentCourse.split(' ')[0].lower()
     	courseCode = currentCourse.split(' ')[1]
 
