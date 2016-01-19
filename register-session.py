e54910d153ce7d2874e3c5b5878f867b4a642032
Shameel Abdullah <shameel.abdullah@gmail.com>
2016-01-19 18:55:33 -0500

    messages on telegram with info sessions for the day

--- register-session.py
+++ register-session.py
@@ -2,6 +2,48 @@ import requests
 import time
 import datetime
 from uwaterlooapi import UWaterlooAPI
+import logging
+import telegram
+from time import sleep
+
+try:
+    from urllib.error import URLError
+except ImportError:
+    from urllib2 import URLError  # python 2
+
+def botMessage(message):
+    # Telegram Bot Authorization Token
+    bot = telegram.Bot("167469051:AAElg9Rdrj1YTwRJxEVh4WpqaGw8GeDDpS0")
+
+    # get the first pending update_id, this is so we can skip over it in case
+    # we get an "Unauthorized" exception.
+    try:
+        update_id = bot.getUpdates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+    try:
+        update_id = echo(bot, update_id,message)
+    except telegram.TelegramError as e:
+        # These are network problems with Telegram.
+        if e.message in ("Bad Gateway", "Timed out"):
+            sleep(1)
+        elif e.message == "Unauthorized":
+            # The user has removed or blocked the bot.
+            update_id += 1
+        else:
+            raise e
+
+
+def echo(bot, update_id, message):
+    bot.sendMessage(chat_id=130724919,text=message)
+    return update_id
+
+
 
 API_KEY = "36802f2c7eab5943ece0bcf8eec07d5a"
 uw = UWaterlooAPI(api_key=API_KEY)
@@ -56,14 +98,24 @@ print "---------------"
 print todays_sched
 print "\n"
 
+message = ""
 
-for index,session in enumerate(todays_sessions):
-	for classIndex,classTime in enumerate(todays_sched):
- 		if session[1] < classTime[0]:
- 			print "STARTS BEFORE CLASS: " + str(classIndex)
- 			print session[0] + " starts at: " + session[1] + " and ends at " + session[2] + "."
- 			print "\n"
- 			break
+for session in todays_sessions:
+	message = message + session[0] + " at " + session[1] + " ends at " + session[2] + "\n"
+
+print "TESTING"
+print message
+
+
+botMessage(message)
+
+# for index,session in enumerate(todays_sessions):
+# 	for classIndex,classTime in enumerate(todays_sched):
+#  		if session[1] < classTime[0]:
+#  			print "STARTS BEFORE CLASS: " + str(classIndex)
+#  			print session[0] + " starts at: " + session[1] + " and ends at " + session[2] + "."
+#  			print "\n"
+#  			break
 
 			
 
