44c8183bd264d23b406a67783ef57afab30ed950
meeoh <shameel.abdullah@gmail.com>
2017-01-12 14:59:31 -0500

    sorted info sessions, removed comments

--- register-session.py
+++ register-session.py
@@ -62,60 +62,23 @@ day = int(time.strftime("%d"))
 
 todaysDate = time.strftime("%Y") + time.strftime("-%m-") + str(day)
 
-
-for event in uw.infosessions():    
+#get all sessions for today
+for event in uw.infosessions():
     if "CANCELLED" in event["employer"].upper():
         continue
-    if event["date"] == todaysDate:        
+    if event["date"] == todaysDate:
         todays_sessions.append(
             (event["employer"], event["start_time"], event["end_time"], event["id"]))
 
-
-'''
-todays_sessions.sort(key=lambda x: x[1])
-print "TODAYS SESSIONS"
-print "---------------"
-print todays_sessions	
-
-weekday = time.strftime("%A").upper()
-
-
-sched = [[] for x in range(5)]
-
-#put the location of sched.txt here
-f = open('/home/pi/Info-Session-Schedueler/sched.txt', 'r')
-currentDay = -1
-for line in f:
-	if "DAY" in line:
-		currentDay = currentDay + 1
-	else:		
-		starting = line.split(' ')[0]
-		ending = line.split(' ')[1].strip()
-		sched[currentDay].append((starting, ending))			
-
-today_as_int = datetime.datetime.today().weekday()
-if today_as_int > 4:
-	today_as_int = 0
-
-todays_sched = sched[datetime.datetime.today().weekday()]
-print "TODAYS CLASSES"
-print "---------------"
-print todays_sched
-print "\n"
-'''
+#sort session by ascending time
+todays_sessions.sort(key=lambda tup: tup[1])  # sorts in place
 
 message = ""
 
+#create the message
 for session in todays_sessions:
     message = message + session[0] + " at " + \
         session[1] + " ends at " + session[2] + "\n"
 
+#send the message
 botMessage(message)
-
-# for index,session in enumerate(todays_sessions):
-# 	for classIndex,classTime in enumerate(todays_sched):
-#  		if session[1] < classTime[0]:
-#  			print "STARTS BEFORE CLASS: " + str(classIndex)
-#  			print session[0] + " starts at: " + session[1] + " and ends at " + session[2] + "."
-#  			print "\n"
-#  			break
